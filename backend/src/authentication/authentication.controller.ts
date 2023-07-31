ceab57882a90c306bbe007cabec93f93a7ec5a6f
TN19N <MostafaAnawawi@gmail.com>
2023-07-31 19:04:44 +0100

    bug: fix redirect in case of UnauthorizedException

--- backend/src/authentication/authentication.controller.ts
+++ backend/src/authentication/authentication.controller.ts
@@ -5,6 +5,7 @@ import {
   HttpStatus,
   Post,
   Res,
+  UseFilters,
   UseGuards,
 } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
@@ -12,11 +13,17 @@ import { ApiTags } from '@nestjs/swagger';
 import { Intra42Guard, JwtGuard } from './guard';
 import { GetUserId } from './decorator';
 import { Response } from 'express';
+import { ConfigurationService } from 'src/configuration/configuration.service';
+import { LoginRedirectFilter } from 'src/common';
 
 @Controller('auth')
 @ApiTags('auth')
+@UseFilters(LoginRedirectFilter)
 export class AuthenticationController {
-  constructor(private authenticationService: AuthenticationService) {}
+  constructor(
+    private authenticationService: AuthenticationService,
+    private configurationService: ConfigurationService,
+  ) {}
 
   @Get('intra42')
   @UseGuards(Intra42Guard)
@@ -25,7 +32,7 @@ export class AuthenticationController {
       userId,
     );
     response.setHeader('Set-Cookie', `Authentication=${loginToken}; Path=/`);
-    response.redirect('/home');
+    response.redirect(this.configurationService.get('FRONTEND_URL'));
   }
 
   @Post('logout')
