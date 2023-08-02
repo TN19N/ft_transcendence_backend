24f37a227294e4a39d7e7aa2ad14a8678c8e3f74
TN19N <MostafaAnawawi@gmail.com>
2023-08-02 14:49:19 +0100

    future: add get user object

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,15 +1,40 @@
-import { Controller, Get, UseFilters, UseGuards } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  NotFoundException,
+  Query,
+  UseFilters,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiQuery, ApiTags } from '@nestjs/swagger';
+import { GetUserId } from 'src/authentication/decorator';
 import { JwtGuard } from 'src/authentication/guard';
 import { LoginRedirectFilter } from 'src/common';
+import { UserService } from './user.service';
+import { UserRepository } from './user.repository';
 
 @Controller('user')
 @ApiTags('user')
 @UseGuards(JwtGuard)
 @UseFilters(LoginRedirectFilter)
 export class UserController {
-  @Get('test')
-  async test() {
-    return 'you are allowed to see this';
+  constructor(
+    private userService: UserService,
+    private userRepository: UserRepository,
+  ) {}
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  @ApiQuery({ name: 'id', required: false })
+  async getUser(@GetUserId() userId: string, @Query('id') id?: string) {
+    const user = await this.userRepository.getUserById(id ?? userId);
+
+    if (user) {
+      return user;
+    } else {
+      throw new NotFoundException(`User with id ${id ?? userId} not found`);
+    }
   }
 }
