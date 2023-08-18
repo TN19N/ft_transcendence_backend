75dab8fb3cdb14973452cb987f536f89000cf087
TN19N <MostafaAnawawi@gmail.com>
2023-08-18 18:57:18 +0100

    improve: make match history api have better return

--- backend/src/game/game.repository.ts
+++ backend/src/game/game.repository.ts
@@ -1,14 +1,33 @@
 import { Injectable } from '@nestjs/common';
-import { GameRecord } from '@prisma/client';
 import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
 export class GameRepository {
   constructor(private databaseService: DatabaseService) {}
 
-  getRecord(userId: string): Promise<GameRecord[]> {
+  getRecord(userId: string) {
     return this.databaseService.gameRecord.findMany({
       where: { userId: userId },
+      include: {
+        user: {
+          select: {
+            profile: {
+              select: {
+                name: true,
+              },
+            },
+          },
+        },
+        opponent: {
+          select: {
+            profile: {
+              select: {
+                name: true,
+              },
+            },
+          },
+        },
+      },
     });
   }
 }
