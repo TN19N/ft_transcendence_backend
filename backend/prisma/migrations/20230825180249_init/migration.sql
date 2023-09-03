4d97038591944d63b58ec6e72094f4e58e75559c
TN19N <MostafaAnawawi@gmail.com>
2023-09-03 12:24:49 +0100

    clean: one prisma migrattion

--- backend/prisma/migrations/20230825180249_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Group" ADD COLUMN     "membersCount" INTEGER NOT NULL DEFAULT 0;
--- backend/prisma/migrations/20230830165828_inti/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "gameInviteToken" TEXT NOT NULL DEFAULT '';
